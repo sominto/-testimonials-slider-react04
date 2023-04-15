a7dc11a18cc5217752972e51b4f5789a69339a58
sominto <charliesominto@gmail.com>
2023-04-15 11:48:08 +0530

    create testimonial slide

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Sominto Kumar",
+    job: "Software Engineer",
+    image: "https://avatars.githubusercontent.com/u/104055424?s=400&u=a38ae81686ffda3e476d01ec89bfc1f9a02298cc&v=4",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Pranay Gupta",
+    job: "Graphic Designer",
+    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Himanshu Relhan",
+    job: "Marketing Manager",
+    image: "https://media.licdn.com/dms/image/D4D35AQGZXM1wkZwzhQ/profile-framedphoto-shrink_100_100/0/1679812812897?e=1682146800&v=beta&t=Yb2D-P51y_6N5l5jtlcrlf01kDzDo8tkyNNgbSD1oJw",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Soumya Banerjee",
+    job: "Content Strategist",
+    image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Atul Joshi",
+    job: "Data Analyst",
+    image: "https://media.licdn.com/dms/image/D4D03AQEqDW48fMYmVw/profile-displayphoto-shrink_400_400/0/1648611159054?e=1686787200&v=beta&t=xRwtXp5fTS_bEuXuR4k9dn9x-UeQkCebJ5HHJuVy14w",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
